9155239828ba769b6c9078ffa3701b4e00e57e69
yjlee-PC\yjlee <yongjin48@gmail.com>
2018-09-02 20:32:06 +0900

    2018 09 02 2-3강까지

---
+++ src/main/java/net/slipp/web/UserController.java
@@ -0,0 +1,27 @@
+package net.slipp.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	private List<User> users = new ArrayList<User>();
+	
+	@RequestMapping("/create")
+	public String create(User user) {
+		System.out.println("User = "+ user);
+		users.add(user);
+		return "redirect:/list";
+	}
+	
+	@RequestMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users",users);
+		return "list";
+	}
+}
