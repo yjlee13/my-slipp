54ba5197a015825fff0e680630f70fc2e1b43f77
tjlee <tmffjtl21@gmail.com>
2018-09-03 19:48:39 +0900

    branch commit

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-	
+	// testetstset
 	private List<User> users = new ArrayList<User>();
 	
 	@RequestMapping("/create")
