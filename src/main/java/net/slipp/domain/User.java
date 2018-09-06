5f16f6b2fb868c9e829ed1a9c5166c8e5dae9d6b
yjlee-PC\yjlee <yongjin48@gmail.com>
2018-09-07 00:24:44 +0900

    3-5강까지

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -1,7 +1,20 @@
-package net.slipp.web;
+package net.slipp.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false, length=20)
 	private String userId;
+	
 	private String password;
 	private String name;
 	private String email;
@@ -33,6 +46,12 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
+	}
+	public void update(User newUser) {
+		this.password = newUser.password;
+		this.name=newUser.name;
+		this.email=newUser.email;
+		
 	}
 	
 	
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,27 +1,64 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
+
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
-	private List<User> users = new ArrayList<User>();
+
+	@Autowired
+	private UserRepository userRepository;
 	
-	@RequestMapping("/create")
+	@GetMapping("/index")
+	public String index() {
+		return "/index";
+	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/form";
+	}
+	
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("User = "+ user);
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 	
-	@RequestMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+		model.addAttribute("users",userRepository.findAll());
+		return "/user/list";
 	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {   //id 맞춰줘야됨 ex) {userid} 이면 Long userid
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateform";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
 }
+
+
+
